84042679c9f9404948d231ba99f600dfac99dd8d
nathanmbebi24 <ngokoangel2015@gmail.com>
2026-07-25 15:18:18 +0100

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+
+    int a, b;
+    char op;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter an operator (+ - * /): ");
+    scanf(" %c", &op);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    if(op=='+')
+        printf("Result = %d", a+b);
+
+    else if(op=='-')
+        printf("Result = %d", a-b);
+
+    else if(op=='*')
+        printf("Result = %d", a*b);
+
+    else if(op=='/')
+        printf("Result = %d", a/b);
+
+    else
+        printf("Invalid operator");
+
+    return 0;
+}
