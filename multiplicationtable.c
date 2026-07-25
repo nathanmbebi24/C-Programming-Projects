d3f9d2ed76b46610c770f169997d32a6c94462b8
nathanmbebi24 <ngokoangel2015@gmail.com>
2026-07-25 15:47:11 +0100

    Add files via upload

---
+++ multiplicationtable.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    int number;
+    int i;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    for(i = 1; i <= 10; i++)
+    {
+        printf("%d x %d = %d\n",
+               number,
+               i,
+               number * i);
+    }
+
+    return 0;
+}
